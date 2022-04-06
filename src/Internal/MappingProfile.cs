65619bf57940dec1806a14dce06b2375c1e0a683
Patrick James <pj997067@gmail.com>
2022-04-06 16:42:47 -0400

    Create mapping profile

---
+++ src/Internal/MappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace AutoPocoIO.Repository.Internal;
+internal class MappingProfile: Profile
+{
+    public MappingProfile(): base(Assembly.GetExecutingAssembly().GetName().Name)
+    {
+        ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        
+    }
+
+    public MappingProfile(Assembly assembly): base(assembly.GetName().Name)
+    {
+        ApplyMappingsFromAssembly(assembly);
+    }
+
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetExportedTypes()
+            .Where(c => typeof(IMapFrom).IsAssignableFrom(c) && !c.IsInterface && !c.IsAbstract);
+
+        foreach (var type in types)
+        {
+            var instance = Activator.CreateInstance(type);
+            var methodInfo = type.GetMethod(nameof(IMapFrom.Mapping));
+            methodInfo!.Invoke(instance, new object[] { this });
+        }
+
+    }
+
+    public override string ToString()
+    {
+        return ProfileName;
+    }
+}
---
+++ tests/AutoPocoIO.Repository.Test/Internal/MappingProfileTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoPocoIO.Repository.Internal;
+using AutoPocoIO.Repository.Test.Services.TestUtilities;
+using Xunit;
+
+namespace AutoPocoIO.Repository.Test.Internal;
+public class MappingProfileTests
+{
+    public  static IEnumerable<object[]> _data => new List<object[]>()
+    {
+         new object[]{ new MappingProfile(), "AutoPocoIO.Repository"},
+         new object []{ new MappingProfile(typeof(MappingProfileTests).Assembly), "AutoPocoIO.Repository.Test" }
+    };
+
+    public class PersonDtoWithMap: IEntityDto, IMapFrom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public void Mapping(Profile profile)
+        {
+           profile.CreateMap<PersonEntity, PersonDtoWithMap>();
+        }
+    }
+
+
+
+
+    [Theory]
+    [MemberData(nameof(_data))]
+    public void AutoMapper_ConfigurationValid(object profile, string profileName)
+    {
+        var mappingProfile = profile as MappingProfile;
+
+        var configuration = new MapperConfiguration(c => c.AddProfile(mappingProfile));
+        configuration.AssertConfigurationIsValid();
+
+        Assert.NotNull(mappingProfile);
+        Assert.Equal(profileName, mappingProfile.ProfileName);
+    }
+
+    [Fact]
+    public void ApplyMappingsFromAssembly_AddsPersonEntiyToDtoMapping()
+    {
+        var configuration = new MapperConfiguration(c => c.AddProfile(new MappingProfile(typeof(MappingProfileTests).Assembly)));
+        var mapper = configuration.CreateMapper();
+
+        PersonEntity entity = new()
+        {
+            Id = 1,
+            Name = "name1"
+        };
+
+        var dto = mapper.Map<PersonDtoWithMap>(entity);
+
+        Assert.Equal(1, dto.Id);
+        Assert.Equal("name1", dto.Name);
+
+    }
+}

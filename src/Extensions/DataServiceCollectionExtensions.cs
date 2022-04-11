14f82da72a1c8ec9083729f2c2e88924b892b8a6
pjames997 <pjames997067@gmail.com>
2022-04-11 11:47:53 -0400

    Add Services for IOC

---
+++ src/Extensions/DataServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using AutoMapper;
+using AutoMapper.Extensions.ExpressionMapping;
+using AutoPocoIO.Repository.Internal;
+using AutoPocoIO.Repository.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutoPocoIO.Repository.Extensions;
+
+public static class DataServiceCollectionExtensions
+{
+    public static IServiceCollection AddGenericMappingServices(
+        this IServiceCollection services,
+        params Assembly[] profileAssemblies)
+    {
+        services.AddScoped(typeof(IRepositoryAsync<>), typeof(RepositoryAsync<>));
+        services.AddTransient(typeof(IRepositoryServiceAsync<,>), typeof(RepositoryServiceAsync<,>));
+        services.AddAutoMapper(c => c.AddExpressionMapping()
+                                      .AddProfileAssemblies(profileAssemblies),
+                                typeof(MappingProfile));
+
+
+        return services;
+    }
+
+    private static IMapperConfigurationExpression AddProfileAssemblies(
+        this IMapperConfigurationExpression expression,
+        params Assembly[] profileAssemblies)
+    {
+
+        foreach (var asm in profileAssemblies)
+        {
+            expression.AddProfile(new MappingProfile(asm));
+        }
+
+        return expression;
+    }
+}
+
